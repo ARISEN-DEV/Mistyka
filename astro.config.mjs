84a06672f361b4d88366c8bbc9e5a0aa4ca7e628
ARISEN-DEV <rfalconeb@gmail.com>
2023-11-20 17:26:26 -0300

    changing colors, bg. Fixing price, fb image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ARISEN-DEV.github.io',
-  base: 'hotel',
-  integrations: [react(), tailwind()]
+
+  build: {
+    assets: 'assets'
+  },
+
+  integrations: [react(), tailwind(), relativeLinks()]
 });
